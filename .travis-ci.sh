0e83bd7c805449af143b2140614fcacbfcbd948d
BrianAdams <brian@babastik.com>
2015-06-19 16:22:21 -0700

    test

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -51,8 +51,9 @@ function setup_arm_chroot {
     # Indicate chroot environment has been set up
     sudo touch ${CHROOT_DIR}/.chroot_is_done
     ls -la ${CHROOT_DIR}/${TRAVIS_BUILD_DIR}
+    sudo chmod 777 ${CHROOT_DIR}/${TRAVIS_BUILD_DIR}
     # Call ourselves again which will cause tests to run
-    sudo chroot ${CHROOT_DIR} bash -c "cd ${TRAVIS_BUILD_DIR} && sudo ./.travis-ci.sh"
+    sudo chroot ${CHROOT_DIR} bash -c "cd ${TRAVIS_BUILD_DIR} && ./.travis-ci.sh"
 }
 
 if [ -e "/.chroot_is_done" ]; then
