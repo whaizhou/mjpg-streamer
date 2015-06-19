f2fd947510d10271c184c4a09472e6580f6bbfe2
BrianAdams <brian@babastik.com>
2015-06-19 09:20:40 -0700

    Initial travis-ci push

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-mjpg-streamer
+mjpg-streamer armhf
 =============
 
-Fork of http://sourceforge.net/projects/mjpg-streamer/
+Fork of http://sourceforge.net/projects/mjpg-streamer/ with travis-ci build of a armhf package for Debian. Currently Jessie.
---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -x
+set -e
+
+#chmod 644 /etc/resolv.conf
+
+#echo '--------- OLD -----------'
+#cat /etc/apt/sources.list
+#echo 'deb ftp://carroll.aset.psu.edu/pub/linux/distributions/debian/ wheezy main contrib non-free' >>  /etc/apt/sources.list
+
+#sed -i 's/ftp.us.debian.org/128.61.240.89/g' /etc/apt/sources.list
+#sed -i 's/security.debian.org/128.101.240.212/g' /etc/apt/sources.list
+#sed -i 's/carroll.aset.psu.edu/128.118.2.96/g' /etc/apt/sources.list
+
+#echo '--------- NEW -----------'
+#cat /etc/apt/sources.list
+
+apt-get install -y \
+g++ \
+cmake \
+build-essential \
+imagemagick \
+pck-config \
+libv4l-dev \
+v4l-utils \
+v4l2ucp \
+libjpeg62-turbo-dev
+
+mkdir -p /tmp/mjpg-streamer_install/usr/local/
+mkdir -p /tmp/mjpg-streamer_install/usr/local/bin
+mkdir -p /tmp/mjpg-streamer_install/usr/local/lib
+
+make install DESTDIR=/tmp/mjpg-streamer_install/usr/local
