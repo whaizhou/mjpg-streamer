231d45788ea9811f68ed9b051708826424f7ec24
BrianAdams <brian@babastik.com>
2015-09-21 22:34:56 -0700

    fix

--- package-deb.sh
+++ package-deb.sh
@@ -19,6 +19,5 @@ cd $DIR
 fpm -f -m info@openrov.com -s dir -t deb -a $ARCH \
 	-n ${PACKAGE} \
 	-v ${PACKAGE_VERSION} \
-  --after-install=${DIR}/install_lib/openrov-avrdude-afterinstall.sh \
 	--description "OpenROV mjpeg-streamer" \
 	-C ${DIR}/tmp/mjpg-streamer_install .
