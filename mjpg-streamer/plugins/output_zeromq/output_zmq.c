c4371bbcd43f1e85447f58ed7d7bbc62fb4af407
codewithpassion <dominik.fretz@gmail.com>
2016-06-28 21:12:14 -0700

    base64 encoding in C

--- mjpg-streamer/plugins/output_zeromq/output_zmq.c
+++ mjpg-streamer/plugins/output_zeromq/output_zmq.c
@@ -93,6 +93,46 @@ void worker_cleanup(void *arg)
     close(fd);
 }
 
+
+static char encoding_table[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                                'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                                'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+                                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                                'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                                'w', 'x', 'y', 'z', '0', '1', '2', '3',
+                                '4', '5', '6', '7', '8', '9', '+', '/'};
+static int mod_table[] = {0, 2, 1};
+
+char *base64_encode(const unsigned char *data,
+                    size_t input_length,
+                    size_t *output_length) {
+
+    *output_length = 4 * ((input_length + 2) / 3);
+
+    char *encoded_data = malloc(*output_length);
+    if (encoded_data == NULL) return NULL;
+
+    for (int i = 0, j = 0; i < input_length;) {
+
+        uint32_t octet_a = i < input_length ? (unsigned char)data[i++] : 0;
+        uint32_t octet_b = i < input_length ? (unsigned char)data[i++] : 0;
+        uint32_t octet_c = i < input_length ? (unsigned char)data[i++] : 0;
+
+        uint32_t triple = (octet_a << 0x10) + (octet_b << 0x08) + octet_c;
+
+        encoded_data[j++] = encoding_table[(triple >> 3 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 2 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 1 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 0 * 6) & 0x3F];
+    }
+
+    for (int i = 0; i < mod_table[input_length % 3]; i++)
+        encoded_data[*output_length - 1 - i] = '=';
+
+    return encoded_data;
+}
+
 int register_camera(char* url) 
 {
     DBG("Registering");
@@ -106,9 +146,10 @@ int register_camera(char* url)
     char payload[(strlen(data) -4) + strlen(name) + strlen(url)];
 
     sprintf(payload, data, name, url);
+
     int payloadSize = strlen(payload);
     
-    zmq_send (registrar, payload, payloadSize, 0);       
+    zmq_send (registrar, payload, payloadSize, 0);
 
     zmq_close (registrar);
     zmq_ctx_destroy (context);
@@ -174,7 +215,11 @@ void *worker_thread(void *args)
 
         DBG("sending frame");
 
-        zmq_send (publisher, (void*)frame, frame_size, 0);        
+        size_t outputL;
+        char *foo = base64_encode((const unsigned char*)frame, frame_size, &outputL);
+
+        zmq_send (publisher, (void*)foo, outputL, 0);
+        free(foo);        
 
     }
 
